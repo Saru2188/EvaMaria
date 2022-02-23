22b5d3cd4274e3a761a87739fc6eff6aaff2da9d
Saru2188 <98587260+Saru2188@users.noreply.github.com>
2022-02-23 15:56:12 +0530

    sarvaish

--- sample_info.py
+++ sample_info.py
@@ -1,23 +1,22 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
-USERBOT_STRING_SESSION = ''
+API_ID = '18168309'
+API_HASH = '57b2582f71b6fe3792c160e86a90b98c'
+BOT_TOKEN = '5298885585:AAHzDymuUclxVgQyeAPxMq7lpeQ-q6iKG3Qz'
 
 # Bot settings
-CACHE_TIME = 300
+CACHE_TIME = 180
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
-AUTH_USERS = []
-AUTH_CHANNEL = None
+ADMINS = [1307461055]
+CHANNELS = [-1001726306787]
+AUTH_USERS = [1307461055]
+AUTH_CHANNEL = [-1001726306787]
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://Saru2188:Saru2188@cluster0.l11up.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 DATABASE_NAME = 'Telegram'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
